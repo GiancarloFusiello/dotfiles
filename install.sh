a6d0477092b328d222d81daf38465de125bbf539
Giancarlo Fusiello <7973166+GiancarloFusiello@users.noreply.github.com>
2023-06-30 17:34:36 +0100

    add install.sh script

---
+++ install.sh
@@ -0,0 +1,17 @@
+# install homebrew package manager
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# install dependencies using homebrew
+brew bundle
+
+# add config files to home directory
+stow stow-global-ignore
+stow git
+stow iterm
+stow startship
+stow zsh
+
+# add languages to asdf
+asdf plugin-add python
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
